4b95e2ac497b8e5bd64091150a1c218d81a9a924
MC <mcarrera00@gmail.com>
2017-01-24 20:55:31 -0600

    Final HW4
    run.bat

--- HomeWork4/HomeWork4/Eventer.py
+++ HomeWork4/HomeWork4/Eventer.py
@@ -19,40 +19,47 @@ def find_events(ls_symbols, d_data):
     # Time stamps for the event range
     ldt_timestamps = df_close.index
     writer = csv.writer(open('orders.csv', 'wb'), delimiter = ',')
-    writer2 = csv.writer(open('list10.csv', 'wb'))
+    # Make a list only based on the threshold
+    #writer2 = csv.writer(open('list10.csv', 'wb'), delimiter = ',')
     
+
     for s_sym in ls_symbols:
-        for i in range(1, len(ldt_timestamps)-5):
+        for i in range(1, len(ldt_timestamps) - 5):
             # Calculating the returns for this timestamp
             f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
             f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
-            f_symprice_5days = df_close[s_sym].ix[ldt_timestamps[i + 5]]
+            #f_symprice_5days = df_close[s_sym].ix[ldt_timestamps[i + 5]]
 
             #print(str(ldt_timestamps[i])+"Symbol:" + s_sym + " Today: " +
             #str(f_symprice_today) + " Yest: " + str(f_symprice_yest))
             # Look for event
-            f_threshold = 10.0
+            f_threshold = 10.00
             if f_symprice_today < f_threshold and f_symprice_yest >= f_threshold:
-                #writer.writerow([ldt_timestamps[i], s_sym, "BUY", "100", f_symprice_today ])
-                #writer.writerow([ldt_timestamps[i] + dt.timedelta(days=5), s_sym, "SELL", "100", f_symprice_5days])
-                writer2.writerow([s_sym]);
+                writer.writerow([ldt_timestamps[i].year, ldt_timestamps[i].month, ldt_timestamps[i].day, s_sym, "BUY", "100"])
+                writer.writerow([ldt_timestamps[i+5].year, ldt_timestamps[i+5].month, ldt_timestamps[i+5].day, s_sym, "SELL", "100"])
+                #writer2.writerow([s_sym]);
 
 
 
        
    
 
-
 if __name__ == '__main__':
     print "Start"
-    dt_start = dt.datetime(2008, 1, 1)
-    dt_end = dt.datetime(2009, 12, 31)
+    dt_start = dt.datetime(2008, 1, 2)
+    dt_end = dt.datetime(2009, 12, 28) + dt.timedelta(days=5)
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
     dataobj = da.DataAccess('Yahoo')
     #get list of symbols from file
-    list = 'sp5002012'
-    ls_symbols = dataobj.get_symbols_from_list(list)
+    ls_symbols = []
+    list = 'sp5002012.txt'
+    #list = 'list5.csv'
+
+    with open(list, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ls_symbols.append(row[0])
     ls_symbols.append('SPY')
 
     ls_keys = ['actual_close']
@@ -67,3 +74,5 @@ if __name__ == '__main__':
     df_events = find_events(ls_symbols, d_data)
     
     print "Done."            
+    print "Run the market Sym."
+    
---
+++ HomeWork4/HomeWork4/run.bat
@@ -0,0 +1,3 @@
+python Eventer.py
+python Market_sim.py
+python Analyzer.py
