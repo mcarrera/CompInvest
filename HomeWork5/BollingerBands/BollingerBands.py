d8241c7081e14b5bf481acddacb3703ca64e6091
MC <mcarrera00@gmail.com>
2017-02-04 10:13:33 -0600

    Bollinger Values

--- HomeWork5/BollingerBands/BollingerBands.py
+++ HomeWork5/BollingerBands/BollingerBands.py
@@ -11,23 +11,27 @@ if __name__ == '__main__':
     print "Start"
     #setup
     lookback = 20
-    dt_start = dt.datetime(2010, 12, 23) 
-    dt_end = dt.datetime(2010, 12, 31) 
+    dt_start = dt.datetime(2010, 6,12) 
+    dt_end = dt.datetime(2010, 6, 29) 
     ldt_timestamps = du.getNYSEdays(dt_start  - dt.timedelta(lookback*3), dt_end, dt.timedelta(hours=16))
     ls_timestamps = []
     
     # get data
     dataobj = da.DataAccess('Yahoo')
-    ls_symbols = ['AAPL'] #, 'GOOG', 'IBM', 'MSFT']
-    ls_keys = ['close']
+    ls_symbols = ['AAPL', 'MSFT']
+    ls_keys = ['actual_close']
     df_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, df_data))
-    df_close = d_data['close']
+    df_close = d_data['actual_close']
     means = pd.rolling_mean(df_close,20,min_periods=20)
-    print means
+    #print means
     stds = pd.rolling_std(df_close, 20)
-    print stds
-             
+    #print stds
+    bol_val = df_close.add(-means, fill_value = 0).div(stds, fill_value=0)
+
+    print bol_val
+    print df_close
+
                 
                     
 
